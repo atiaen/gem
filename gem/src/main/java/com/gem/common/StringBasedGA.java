bcc27cf6930c13d574cef0a703a1698c5437dc86
Adeayo Oladipo <adeayooladipo@gmail.com>
2023-10-03 22:49:16 +0100

    some more restructuring

--- gem/src/main/java/com/gem/common/StringBasedGA.java
+++ gem/src/main/java/com/gem/common/StringBasedGA.java
@@ -31,42 +31,46 @@ public class StringBasedGA {
 
         Integer generationCount = 0;
 
-        //Find both fittest and best fit individual in current population
+        // Find both fittest and best fit individual in current population
         Individual fittestIndividualInCurrentGen = p.findFittestIndividual();
         Individual mostFitIndividual = p.findFittestIndividual();
 
-        //Loop while our current generation is less than max generations
+        // Loop while our current generation is less than max generations
         while (generationCount <= maxGenerations - 1) {
             generationCount++;
-
 
             fittestIndividualInCurrentGen = p.findFittestIndividual();
             if (fittestIndividualInCurrentGen.getInFitness() > mostFitIndividual.getInFitness()) {
                 mostFitIndividual = fittestIndividualInCurrentGen;
             }
 
+            System.out.println("Target:               " + App.target.toString());
+            System.out.println("Best Individual:      " + mostFitIndividual.getIndividualString());
+            System.out.println("Current Generation:      " + generationCount);
+
             // If we find the target stop loop and print out here
             if (fittestIndividualInCurrentGen.getIndividualString().equals(App.target)) {
                 System.out.println("Hit the Target!!");
-                System.out.println(String.format("Target:               " + App.target.toString()));
-                System.out.println(String.format("Best Individual:      " + mostFitIndividual.getIndividualString()));
-                System.out.println(String.format("Generation Found:      " + generationCount));
+                System.out.println("Target:               " + App.target.toString());
+                System.out.println("Best Individual:      " + mostFitIndividual.getIndividualString());
+                System.out.println("Generation Found:      " + generationCount);
                 break;
             }
 
             // If we reach max generation, print missed target and other info
             if (generationCount.equals(maxGenerations)) {
                 System.out.println("Missed the target...");
-                System.out.println(String.format("Target:                " + App.target));
-                System.out.println(String.format("Best Individual:       " + mostFitIndividual.getIndividualString()));
-                System.out.println(String.format("Generation Found:     " + generationCount));
-                System.out.println(String.format("Score: " + mostFitIndividual.getIndividualFitness()));
+                System.out.println("Target:                " + App.target);
+                System.out.println("Best Individual:       " + mostFitIndividual.getIndividualString());
+                System.out.println("Generation Found:     " + generationCount);
+                System.out.println("Score: " + mostFitIndividual.getIndividualFitness());
             }
 
-            // If above conditions fail. Generate another population based on previous population and set new pop to current pop.
+            // If above conditions fail. Generate another population based on previous
+            // population and set new pop to current pop.
             Population offsPopulation = utils.createNewGeneration(p);
             p = offsPopulation;
-            
+
         }
 
     }
